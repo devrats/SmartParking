be848bb84ae0ab7a8f38d5ed7905de064db791ee
Sharma Ji <devvrat.sharm_cs19@gla.ac.in>
2021-08-29 15:42:22 +0530

    fixed typos

--- app/src/main/java/com/example/smartparking/MainActivity.java
+++ app/src/main/java/com/example/smartparking/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
 
     private Button button;
     private Button button1;
-    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +30,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),MainActivity3.class);
                 startActivity(intent);
-            }
-        });
-
-        button3 = findViewById(R.id.button9);
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MainActivity5.class));
             }
         });
     }
